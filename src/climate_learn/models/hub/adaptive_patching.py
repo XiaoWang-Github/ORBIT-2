e3241c3deb4bc7016793b8d572a2c48673a857b7
Isaac <irlyngaas@github.com>
2025-02-25 10:28:37 -0500

    Adding second derivative version of PIAP

--- src/climate_learn/models/hub/adaptive_patching.py
+++ src/climate_learn/models/hub/adaptive_patching.py
@@ -27,16 +27,25 @@ class Patchify(torch.nn.Module):
         if self.physics:
             #To Gaussian Filter or not??
             #grey_img = cv.GaussianBlur(img, (self.smooth_factor, self.smooth_factor), 0)
+            #Option 1
             #grad = np.gradient(np.squeeze(img))
-            #grad_mag = np.sqrt(grad[0]**2 + grad[1]**2)
-            grad_mag = scipy.ndimage.filters.laplace(np.squeeze(img))
-            ind = np.unravel_index(np.argsort(grad_mag, axis=None), grad_mag.shape)
+            #grad_out = np.sqrt(grad[0]**2 + grad[1]**2)
+
+            #Option 2
+            #grad_out = scipy.ndimage.filters.laplace(np.squeeze(img))
+
+            #Option 3
+            grad = np.gradient(np.squeeze(img))
+            grad_mag = np.sqrt(grad[0]**2 + grad[1]**2)
+            grad_x = np.gradient(grad[0]/grad_mag, axis=0)
+            grad_y = np.gradient(grad[1]/grad_mag, axis=1)
+            grad_out = grad_x + grad_y
+
+            ind = np.unravel_index(np.argsort(grad_out, axis=None), grad_out.shape)
             edges = np.zeros((img.shape[0],img.shape[1]), dtype=np.uint8)
             topK = int(img.shape[0]*img.shape[1]*self.edge_percentage)
             for i in range(img.shape[0]*img.shape[1]-topK, img.shape[0]*img.shape[1]):
                 edges[ind[0][i],ind[1][i]] = 255
-            #H = scipy.ndimage.filters.laplace(np.squeeze(img))
-            #edges
         else:
             if self.smooth_factor ==0 :
                 edges = np.random.uniform(low=0,high=1,size=(img.shape[0],img.shape[1]))
@@ -49,7 +58,6 @@ class Patchify(torch.nn.Module):
         seq_img, seq_size, seq_pos = qdt.serialize(img, size=(self.patch_size,self.patch_size,self.num_channels))
         seq_size = np.asarray(seq_size)
         seq_img = np.asarray(seq_img, dtype=np.float32)
-        #print("SEQ_IMG_SHAPE", seq_img.shape,flush=True)
 
         if self.num_channels > 1:
             seq_img = np.reshape(seq_img, [self.num_channels, -1, self.patch_size*self.patch_size])
